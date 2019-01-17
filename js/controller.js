45ec4d2d6531d3066808a85a4372eb6136e12c64
Shalin Fernando <s98fernando@gmail.com>
2019-01-17 14:28:52 +0530

    Added EcmaScript

---
+++ js/controller.js
@@ -0,0 +1,7 @@
+
+$("#btnSave").click(function () {
+    var id = $("#txtID").val();
+    var name = $("#txtName").val();
+    var address = $("#txtAddress").val();
+    $("#tblCustomer").append("<tr><td>" + id + "</td><td>" + name + "</td><td>" + address + "</td></tr>");
+});
