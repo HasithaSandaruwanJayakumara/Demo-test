a2e8aa2851a3bbc310d08508c409226c0b63bd53
Shalin Fernando <s98fernando@gmail.com>
2019-01-17 14:34:00 +0530

    Added altered Ecma Script

--- js/controller.js
+++ js/controller.js
@@ -4,4 +4,8 @@ $("#btnSave").click(function () {
     var name = $("#txtName").val();
     var address = $("#txtAddress").val();
     $("#tblCustomer").append("<tr><td>" + id + "</td><td>" + name + "</td><td>" + address + "</td></tr>");
+    
+    $("#txtID").val("");
+    $("#txtName").val("");
+    $("#txtAddress").val("");
 });
