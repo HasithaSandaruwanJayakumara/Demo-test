9c29b4b6018e2e671434a777a94f999a3567f81a
Shalin Fernando <s98fernando@gmail.com>
2019-01-17 15:05:58 +0530

    Table Selection

--- js/controller.js
+++ js/controller.js
@@ -4,8 +4,37 @@ $("#btnSave").click(function () {
     var name = $("#txtName").val();
     var address = $("#txtAddress").val();
     $("#tblCustomer").append("<tr><td>" + id + "</td><td>" + name + "</td><td>" + address + "</td></tr>");
-    
+
     $("#txtID").val("");
     $("#txtName").val("");
     $("#txtAddress").val("");
+
+    $("#tblCustomer tr").click(function () {
+        var cstid = $(this).children().first().text();
+        var cstname = $(this).find("td:nth-child(2)").text();
+        var cstaddress = $(this).children().last().text();
+
+        $("#txtID").val(cstid);
+        $("#txtName").val(cstname);
+        $("#txtAddress").val(cstaddress);
+        
+        
+        $("#btnDelete").click(function(){
+            for(i=0; i<$("#tblCustomer").length;i++){
+                var cstid = $("tblCustomer").children().children().first().text();
+                var id = $("#txtID").val();
+                alert(cstid)
+                if(id==cstid){
+                    alert("Working");
+                }
+            }
+            $("#txtID").val("");
+            $("#txtName").val("");
+            $("#txtAddress").val("");
+        });
+
+    })
 });
+
+
+
